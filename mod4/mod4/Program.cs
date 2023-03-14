c2635d84e82f35e74c9fd4e45b9553d7063fdf2a
PC33-IFLAB <PC33-IFLAB>
2023-03-14 17:17:40 +0700

    menambahkan class2

--- mod4/mod4/Program.cs
+++ mod4/mod4/Program.cs
@@ -8,8 +8,55 @@ namespace mod4
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
+
+        public class Buah {
+            public enum buah { Apel, aprikot, alpukat, pisang, paprika, blackberry, ceri, kelapa, jagung, kurma, durian, anggur, melon, semangka }
+
+            public static string getKodeBuah(buah buah)
+            {
+                string[] kodebuah = { "A00", "B00", "C00", "D00", "E00", "F00", "G00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
+                return kodebuah[(int)buah];
+            }
+
         }
+
+        public class posisikarakterGame{
+            enum state { Terbang, Jongkok };
+
+            public static void Main(string[] args)
+            {
+               
+                Console.WriteLine("tombolx tombolw");
+
+                state gerak = state.Terbang;
+                String perintah = "tombolx ditekan";
+                Console.WriteLine("masukan perintah :");
+
+                perintah = Console.ReadLine();
+                
+                while (perintah == "tombolx ditekan" || perintah == "tombolw ditekan")
+                {
+                   
+                    if (perintah == "tombolx ditekan")
+                    {
+                        gerak = state.Terbang;
+                        Console.WriteLine("posisi landing");
+                    }
+                    else if (perintah == "tombolw ditekan"){
+                        gerak = state.Jongkok;
+                        Console.WriteLine("Posisi takeoff");
+                    }
+
+                } 
+                
+            }
+
+
+
+        }
+       
+
+
     }
 }
+
